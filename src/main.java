d977cf006ae45554af8f799db64b141b2a21acaa
EloySPAIN <yole.tres@gmail.com>
2022-11-30 17:32:07 +0100

    Fibonacci en hilo

--- src/main.java
+++ src/main.java
@@ -1,9 +1,24 @@
 
-public class main {
+public class main extends Thread {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		main thread = new main();
+		thread.start();
 
+	}
+
+	public void run() {
+		int num1 = 0, num2 = 1, suma = 1;
+		System.out.println(num1);
+
+		for (int i = 1; i < 10; i++) {
+			System.out.println(suma);
+
+			suma = num1 + num2;
+			num1 = num2;
+			num2 = suma;
+
+		}
 	}
 
 }
